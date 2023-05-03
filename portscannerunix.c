252ab1feddbc354fd924725e4006e1f59d1cd176
Matt Primpas <matthewprimpas@gmail.com>
2023-05-03 08:19:47 -0400

    make Windows compatible scanner

--- portscannerunix.c
+++ portscannerunix.c
@@ -5,8 +5,7 @@
 #include "string.h"
 #include "stdlib.h"
  
-int main(int argc , char **argv)
-{
+int main(int argc , char **argv) {
     struct hostent *host;
     int err, i , sock ,start , end;
     char hostname[100];
@@ -29,37 +28,32 @@ int main(int argc , char **argv)
     sa.sin_family = AF_INET;
      
     //direct ip address, use it
-    if(isdigit(hostname[0]))
-    {
+    if(isdigit(hostname[0])) {
         printf("Doing inet_addr...");
         sa.sin_addr.s_addr = inet_addr(hostname);
         printf("Done\n");
     }
     //Resolve hostname to ip address
-    else if( (host = gethostbyname(hostname)) != 0)
-    {
+    else if( (host = gethostbyname(hostname)) != 0) {
         printf("Doing gethostbyname...");
         strncpy((char*)&sa.sin_addr , (char*)host->h_addr , sizeof sa.sin_addr);
         printf("Done\n");
     }
-    else
-    {
+    else {
         herror(hostname);
         exit(2);
     }
      
     //Start the port scan loop
     printf("Starting the portscan loop : \n");
-    for( i = start ; i <= end ; i++) 
-    {
+    for( i = start ; i <= end ; i++) {
         //Fill in the port number
         sa.sin_port = htons(i);
         //Create a socket of type internet
         sock = socket(AF_INET , SOCK_STREAM , 0);
          
         //Check whether socket created fine or not
-        if(sock < 0) 
-        {
+        if(sock < 0) {
             perror("\nSocket");
             exit(1);
         }
@@ -67,14 +61,12 @@ int main(int argc , char **argv)
         err = connect(sock , (struct sockaddr*)&sa , sizeof sa);
          
         //not connected
-        if( err < 0 )
-        {
+        if( err < 0 ) {
             //printf("%s %-5d %s\r" , hostname , i, strerror(errno));
             fflush(stdout);
         }
         //connected
-        else
-        {
+        else {
             printf("%-5d open\n",  i);
         }
         close(sock);
